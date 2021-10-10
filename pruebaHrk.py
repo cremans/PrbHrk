2ed3225c4c61c5ec94491b3c94657b0e383c8c6e
cremans <88071871+cremans@users.noreply.github.com>
2021-10-10 05:31:05 +0000

    Add files via upload

---
+++ pruebaHrk.py
@@ -0,0 +1,26 @@
+import time
+import psutil
+
+print("Numero de nucleos CPU: " + str(psutil.cpu_count()))
+print("Numero de Threads: " + str(psutil.cpu_count(logical=False)))
+
+dat_freq = psutil.cpu_freq()
+print("Frecuencia del CPU: %.2f" %str(dat_freq.current/1000))
+dat_ram = psutil.virtual_memory()
+print("Memoria RAM: 5.2f" %str(dat_ram.total/1024/1024/1024))
+
+print("Particiones de disco: \n\n")
+particiones = psutil.disk_partitions()
+
+for particion in particiones:
+    print("Device: " + particion.device)
+    print("Mountpoint: " + particion.mountpoint)
+    dat_almc = psutil.disk_usage(particion.mountpoint)
+    print("Almacenamiento Total: %.2f GB" %str(dat_almc.total/1024/1024/1024))
+    print("Almacenamiento Usado: %.2f GB" %str(dat_almc.used/1024/1024/1024))
+    print("Almacenamiento Libre: %.2f GB" %str(dat_almc.free/1024/1024/1024))
+    print("\n")
+
+for i in range(20):
+    print(str(i))
+    time.sleep(1)
